e5ed4d15252608b4d9fee4bf460edfb71157ea9e
Deepak Yadav <deepak.y@activa.one>
2022-11-20 22:07:04 +0530

    Created beta version v0.0.1

--- main.py
+++ main.py
@@ -1,48 +1,57 @@
-from src.hypertune import GeneticAlgorithmSearch as GA
-from src.hypertune import Hparams
+from src.hypertune import GASearch, Hparams
 import tensorflow as tf
 from tensorflow import keras
 
-(img_train, label_train), (img_test, label_test) = keras.datasets.fashion_mnist.load_data()
+(img_train, label_train), (
+    img_test,
+    label_test,
+) = keras.datasets.fashion_mnist.load_data()
 
 # define search parameters
 ht = Hparams()
-hp_units = ht.Int('units', min_value=32, max_value=512, step=32)
-hp_learning_rate = ht.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4])
-hp_activation = ht.Choice('activation', values=['relu', 'sigmoid', 'tanh'])
+hp_units = ht.Int("units", min_value=32, max_value=512, step=32)
+hp_learning_rate = ht.Choice("learning_rate", values=[1e-2, 1e-3, 1e-4])
+hp_activation = ht.Choice("activation", values=["relu", "sigmoid", "tanh"])
 
-# define model 
+# define model
 params = [hp_units, hp_learning_rate, hp_activation]
+
+
 def model_builder(params):
     model = keras.Sequential()
     model.add(keras.layers.Flatten(input_shape=(28, 28)))
     model.add(keras.layers.Dense(units=params[0], activation=params[2]))
     model.add(keras.layers.Dense(10))
-    model.compile(optimizer=keras.optimizers.Adam(learning_rate=params[1]),
-                    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                    metrics=['accuracy'])
+    model.compile(
+        optimizer=keras.optimizers.Adam(learning_rate=params[1]),
+        loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=["accuracy"],
+    )
     return model
 
-tuner = GA(model_builder,
-            objective='val_accuracy',
-            max_epochs=10,
-            directory='my_dir',
-            project_name='intro_to_kt')
 
-stop_early = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
-                     
-tuner.search(img_train, label_train, params, epochs=2, validation_split=0.2, callbacks=[stop_early])
+tuner = GASearch(
+    model_builder=model_builder,
+    params=params,
+    objective="loss",
+    weights=(-1.0,),
+    max_epochs=10,
+    directory="my_dir",
+    project_name="intro_to_kt",
+)
+
+tuner.search(img_train, label_train, epochs=2, validation_split=0.2)
 
 # Get the optimal hyperparameters
-best_hps=tuner.get_best_hyperparameters(num_trials=1)[0]
+best_hps = tuner.get_best_hyperparameters()[0]
 
 # Build the model with the optimal hyperparameters and train it on the data for 50 epochs
 model = tuner.build(best_hps)
-history = model.fit(img_train, label_train, epochs=5, validation_split=0.2)
+history = model.fit(img_train, label_train, epochs=2, validation_split=0.2)
 
-val_acc_per_epoch = history.history['val_accuracy']
+val_acc_per_epoch = history.history["val_accuracy"]
 best_epoch = val_acc_per_epoch.index(max(val_acc_per_epoch)) + 1
-print('Best epoch: %d' % (best_epoch,))
+print("Best epoch: %d" % (best_epoch,))
 
 eval_result = model.evaluate(img_test, label_test)
 print("[test loss, test accuracy]:", eval_result)
--- src/Hypertune.py
+++ src/Hypertune.py
@@ -1,67 +1,99 @@
-from json import tool
 import random
 from deap import creator, base, tools, algorithms
 
 
-class GeneticAlgorithmSearch:
-
-    def __init__(self, model_builder, objective, max_epochs, directory, project_name):
+class GASearch:
+    def __init__(
+        self,
+        model_builder,
+        params,
+        objective,
+        weights=(1.0,),
+        pop_size=5,
+        gen=5,
+        max_epochs=10,
+        directory="\home",
+        project_name="search_hyperparam",
+    ):
         self.model_builder = model_builder
-        self.objective = objective
+        self.params = params
+        self.objective = objective  # ('loss', 'accuracy', 'val_loss', 'val_accuracy')
+        self.weights = weights  # (1.0, -1.0)
+        self.pop_size = pop_size
+        self.gen = gen
         self.max_epochs = max_epochs
         self.directory = directory
         self.project_name = project_name
-        self.best_hp = None
+        self.best_pop = None
 
-    def search(self, x_train, y_label, params, epochs, validation_split, callbacks):
-        self._search_hyperparameter(x_train, y_label, params, epochs, validation_split)
-
-    def get_best_hyperparameters(self, num_trials=1):
-        return self.best_hp
-
-    def build(self, best_hps):
-        return self.model_builder(best_hps)
-    
-    def _search_hyperparameter(self, x_train, y_label, params, epochs, validation_split):
-        creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-        creator.create("Individual", list, fitness=creator.FitnessMax)
-
-        def _eval_val_accuracy(offspring):
-            model = self.model_builder(offspring)
-            history = model.fit(x_train, y_label, epochs=epochs, validation_split=validation_split, verbose=2)
-            val_acc_per_epoch = history.history['val_accuracy']
-            return max(val_acc_per_epoch),
-
-        def _individual():
-            return creator.Individual([x() for x in params])
-
+    def search(
+        self,
+        x_train=None,
+        y_train=None,
+        batch_size=None,
+        epochs=2,
+        verbose="auto",
+        callbacks=None,
+        validation_split=0.2,
+        validation_data=None,
+        shuffle=True,
+        class_weight=None,
+        sample_weight=None,
+        initial_epoch=0,
+        steps_per_epoch=None,
+        validation_steps=None,
+        validation_batch_size=None,
+        validation_freq=1,
+        max_queue_size=10,
+        workers=1,
+        use_multiprocessing=False,
+    ):
+        creator.create("Fitness", base.Fitness, weights=self.weights)
+        creator.create("Individual", list, fitness=creator.Fitness)
         toolbox = base.Toolbox()
-        toolbox.register("individual", _individual)
+
+        individual = lambda: creator.Individual([param() for param in self.params])
+
+        def objective_fn(params):
+            model = self.model_builder(params)
+            hist = model.fit(
+                x_train, y_train, epochs=epochs, validation_split=validation_split
+            )
+            best_score = max(hist.history[self.objective])
+            return (best_score,)
+
+        toolbox.register("individual", individual)
         toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
-
-        toolbox.register("evaluate", _eval_val_accuracy)
+        toolbox.register("evaluate", objective_fn)
         toolbox.register("mate", tools.cxTwoPoint)
         toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
         toolbox.register("select", tools.selTournament, tournsize=3)
 
-        population = toolbox.population(n=10)
-        print(f"INITIAL POPULATIONS: {population}")
+        population = toolbox.population(n=self.pop_size)
+        print(f"INITIAL POPULATIONS: {population}\n")
 
-        NGEN=5
+        NGEN = self.gen
         for gen in range(NGEN):
-            print(f"GENERATION:{gen} STARTED")
             offspring = algorithms.varAnd(population, toolbox, cxpb=0.5, mutpb=0.1)
-            print(f"GENERATION:{gen} POPULATIONS: {offspring}")
             fits = toolbox.map(toolbox.evaluate, offspring)
             for fit, ind in zip(fits, offspring):
-                print(f"HP: {ind}, SCORE: {fit}")
+                print(f"GENERATION:{gen}, HP:{ind}, SCORE:{fit}\n")
                 ind.fitness.values = fit
-            print(f"GENERATION {gen} COMPLETED, BEST_SCORE: {tools.selBest(population, k=1)}, BEST_HP: {tools.selBest(population, k=1)}")
+            print(
+                f"GENERATION {gen} COMPLETED, BEST_HP: {tools.selBest(population, k=1)}\n\n"
+            )
             population = toolbox.select(offspring, k=len(population))
-        
+
         print(f"FINAL HP: {tools.selBest(population, k=len(population))}")
-        self.best_hp = tools.selBest(population, k=1)
+        self.best_pop = tools.selBest(population, k=1)
+
+    def get_best_hyperparameters(self):
+        return self.best_pop
+
+    def build(self, params):
+        model = self.model_builder(params)
+        return model
 
 
 class Hparams:
@@ -71,18 +103,7 @@ class Hparams:
     def Int(self, param_name, min_value, max_value, step):
         self.tools.register(param_name, random.randrange, min_value, max_value, step)
         return self.tools.__dict__[param_name]
-    
+
     def Choice(self, param_name, values):
         self.tools.register(param_name, random.choice, values)
         return self.tools.__dict__[param_name]
-
-
-if __name__ == "__main__":
-    ht = Hparams()
-    # define search parameters
-    hp_units = ht.Int('units', min_value=32, max_value=512, step=32)
-    hp_learning_rate = ht.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4])
-    hp_activation = ht.Choice('activation', values=['relu', 'sigmoid', 'tanh'])
-    print(type(hp_units))
-    params = [hp_units(), hp_learning_rate(), hp_activation()]
-    print(params)
